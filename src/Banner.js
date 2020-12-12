8d7dd30992d0eb948b4c3fa31fd21a99dc3341cd
unknown <fanimokunbusayo@gmail.com>
2020-12-12 23:04:48 +0100

    building banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React , {useState , useEffect} from 'react';
 import axios from './axios';
 import requests  from './requests';
+import  './Banner.css';
 function Banner() {
 const [movie, setMovie] = useState([]);
 
@@ -27,14 +28,14 @@ useEffect(() => {
 
            
             {/* title */}
-         <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+         <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
             {/* div >2 > buu=ttons */}
 
             <div className="banner__buttons">
              <button className="banner__button">Play</button>
             <button className="banner__button">My List</button>
 
-            <h1 className="banner_description">{movie?.overview}</h1>
+            <h1 className="banner__description">{movie?.overview}</h1>
             {/* description */}
            </div>
          </div>
